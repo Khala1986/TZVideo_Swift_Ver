67530cd767350a931b710bbca0fe11bce26a521c
陈学明 <>
2018-10-26 22:18:56 +0800

    config  HomeCollectionViewProxy, HomeTableViewProxy

--- TZVideoSwift/UI/Home/ViewProxy/HomeCollectionViewProxy.swift
+++ TZVideoSwift/UI/Home/ViewProxy/HomeCollectionViewProxy.swift
@@ -7,6 +7,32 @@
 
 import UIKit
 
-class HomeCollectionViewProxy: TZCollectionViewProxy {
+class HomeCollectionViewProxy: TZCollectionViewProxy, TZSegmentedControlDelegate{
 
+    lazy var segmentedControl:TZSegmentedControl = {
+        let segmentedControl = TZSegmentedControl(frame: CGRect.zero, itemwidth: 80, itemTitle:[String]())
+        segmentedControl.sakura.barTintColor()("App.NavigationBarBarTintColor");
+        segmentedControl.sakura.tintColor()("App.NavigationBarTintColor");
+        segmentedControl.normalColor = UIColor.white
+        segmentedControl.highlightColor = UIColor(hex: 0xFCCA07)
+        segmentedControl.delegate = (self as! UIToolbarDelegate)
+        return segmentedControl
+    }()
+    
+    convenience init (identifier: String, configClosure: @escaping TZCVConfigClosures, actionClosure: @escaping TZCVActionClosures){
+        self.init()
+        self.collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: identifier)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+        if width > 0 {
+            let index = scrollView.contentOffset.x / width
+            segmentedControl.didMoveTo(index: index)
+        }
+    }
+    
+    func didSelectItemAtIndex(segmentedControl: TZSegmentedControl, selectedIndex: Int, fromIndex: Int) {
+        self.collectionView.scrollToItem(at: IndexPath(item: selectedIndex, section: 0), at: .centeredHorizontally, animated: true)
+    }
 }
--- TZVideoSwift/UI/Home/ViewProxy/HomeTableViewProxy.swift
+++ TZVideoSwift/UI/Home/ViewProxy/HomeTableViewProxy.swift
@@ -7,6 +7,55 @@
 
 import UIKit
 
-class HomeTableViewProxy: TZTableViewProxy {
+public let HomeTableViewCellIdentifiers:Dictionary<String,String> = [
+    "banner":"HomeBannerTableViewCell",
+    "title":"HomeTitleTableViewCell",
+    "one":"HomeOneBigTableViewCell",
+    "two":"HomeTwoTableViewCell",
+    "change":"HomeChangeTableViewCell",
+    "rank": "HomeRankTableViewCell"]
 
+public let HomeTableViewCellHeights:Dictionary<String,CGFloat> = [
+    "banner": 150*UIScreen.main.bounds.size.width/320,
+    "title": 40,
+    "one": 250*UIScreen.main.bounds.size.width/375,
+    "two": 127*UIScreen.main.bounds.size.width/320,
+    "change": 40,
+    "rank": 210*UIScreen.main.bounds.size.width/375]
+
+class HomeTableViewProxy: TZTableViewProxy {
+    
+    convenience init( identifier: String, configClosure: @escaping TZTVConfigClosures, actionClosure: @escaping TZTVActionClosures){
+        self.init()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        for className in HomeTableViewCellIdentifiers.values{
+            self.tableView.register(UINib.init(nibName: className, bundle: nil), forCellReuseIdentifier: className)
+        }
+        
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let item = self.dataArray[indexPath.row] as! HomeTemplateItem
+        return HomeTableViewCellHeights[item.templateName]!
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        cellActionClosure(tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! TZTableViewCell, dataArray?[indexPath.row] as AnyObject, indexPath)
+    }
+    
+    //MARK: UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = self.dataArray[indexPath.row] as! HomeTemplateItem
+        let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCellIdentifiers[item.templateName]!, for: indexPath)
+        cellConfigClosure(cell as! TZTableViewCell, dataArray?[indexPath.row] as AnyObject,indexPath)
+        
+        return cell;
+    }
+
+    
 }
